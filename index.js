22b0fcd0af45bc10a95dea25a8c88d03bdb1cc09
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-12 22:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,17 +12,3 @@ function showRepositories(event,data){
   '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
-
-function getCommits(el){
-  const name = el.dataset.repo
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
-  req.open("GET", 'https://api.github.com/repos/jeffliu0/' + name + '/commits')
-  req.send()
-}
-
-function showCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
-}
