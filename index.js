70e761faf7d4f1b7224f0f95c3853eb25a070053
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-12 22:33:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRepositories(){
 function showRepositories(event,data){
   //this is set to the XMLHttpRequest object that fired the even
   var repos = JSON.parse(this.responseText)
-      alert()
+  alert(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name +
   '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
