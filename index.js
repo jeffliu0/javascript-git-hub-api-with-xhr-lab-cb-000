05e2bc972be3534b2700d8ffb78da735deb06180
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-12 22:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function getRepositories(){
   const req = new XMLHttpRequest()
   req.addEventListener("submit", showRepositories)
   req.open("GET", "https://api.github.com/users/jeffliu0/repos")
-      alert(req.statusText)
   req.send()
 }
 
